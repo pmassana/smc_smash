88386f419d9a9020178839bfa477c60aa2eafd00
Massana Zapata <pm00518@ssh02.eps.surrey.ac.uk>
2020-01-25 20:57:11 +0000

    Some changes.

--- cmd_isochrones_smc.py
+++ cmd_isochrones_smc.py
@@ -28,34 +28,47 @@ def dereddener(mag, filter_index, EBV, field_number, filterset = 'decam'):
     else:
         return mag - extinction_coeff_sdss[filter_index]*EBV
 
-def isochrone_plotter(isochrone_points, mag_g, mag_i, distance_modulus, field_number):
+def isochrone_plotter(isochrone_points, mag_g, mag_i, distance_modulus, field_number, radius, PA):
     plt.rc('text', usetex=True)
     plt.rc('font', family='serif')
-    plt.rcParams.update({'font.size':16})
-    fig = plt.figure(1, figsize=(5,5))
+    plt.rcParams.update({'font.size':10})
+    fig = plt.figure(1, figsize=(4,5))
     ax = fig.add_subplot(111)
 
-    ax.scatter(isochrone_points[0], isochrone_points[1]+distance_modulus, zorder = 10, c='red', s=4.,
-    label='PARSEC isochrone \n [Fe/H] = -1 \n log Age = 9.8')
+    ax.scatter(isochrone_points[0], isochrone_points[1]+distance_modulus, zorder = 10, c='red', s=3.,
+        label='PARSEC isochrone')
+#    ax.plot(isochrone_points[0], isochrone_points[1]+distance_modulus, zorder = 10, c='red', lw=4.,
+#        label='PARSEC isochrone')
 
     #ax.hist2d(mag_g[mask_bool] - mag_i[mask_bool], mag_g[mask_bool], bins = [xbins, ybins], cmin = 1, zorder = 1)
-    ax.hist2d(mag_g - mag_i, mag_g, bins = [xbins, ybins], cmin = 1, zorder = 1, cmap='Greys',
-    norm=colors.LogNorm(), label = 'SMASH')
-    #vertex_list = [list(x) for x in vertex_coord_2]
+    counts, xedges, yedges, imag = ax.hist2d(mag_g - mag_i, mag_g, bins = [xbins, ybins], cmin = 1, zorder = 1, cmap='Greys',
+        norm=colors.LogNorm(), label = 'SMASH')
+
+    ax.annotate(f'Field {field_number} \n $r = {radius:.1f}$ \n PA$\,={PA:.1f}$', (1.2,16.5))
+    #vertex_list = [list(x) for x in vertex_coord]
     #vertex_array = np.array(vertex_list)
     #polygon = matplotlib.patches.Polygon(vertex_array, alpha = 0.3, edgecolor = 'black', facecolor = 'orange', linestyle = '-', linewidth = '2.5', zorder = 2)
     #ax.add_patch(polygon)
     ax.set_xlabel('$g-i$')
     ax.set_ylabel('$g$')
     plt.gca().invert_yaxis()
+    #cbax = fig.add_axes([0.05,0.9,0.8,0.05])
+    plt.colorbar(imag, orientation = 'horizontal', panchor=(0.5, 0.0), aspect=90)
     plt.minorticks_on()
-    plt.legend()
+    ax.legend(loc='upper left')
     #plt.grid(which='both')
-    plt.savefig('/user/HS128/pm00518/Documents/PhD/CMD_SMASH/Plots/CMD_%s_isochrone.pdf' % (field_number), bbox_inches='tight')
+    plt.savefig('/user/HS128/pm00518/Documents/PhD/CMD_SMASH/Plots/CMD_%s_isochrone.pdf' % (field_number), bbox_inches='tight', dpi=100)
     plt.close()
 
 
-field_list = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','139','141','142','143','144','145','147','149','150','176','177','178','181']
+#field_list = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','139','141','142','143','144','145','147','149','150','176','177','178','181']
+#field_list = ['13']
+field_list_file = np.loadtxt('/user/HS128/pm00518/Documents/PhD/CMD_SMASH/SB_data_errors_dartmouth_decam.dat')
+#print(field_list.T)
+#sys.exit()
+field_list = field_list_file.T[0].astype(int)
+field_radius = field_list_file.T[1]
+field_PA = field_list_file.T[2]
 
 xbinsize = 0.02
 ybinsize = 0.06
@@ -70,7 +83,7 @@ extinction_coeff_decam = np.array([3.9631, 3.1863, 2.1401, 1.5690, 1.1957, 1.047
 isochrones_data = np.loadtxt('/scratch/Isochrones/PARSEC_table.dat')
 
 metallicity = -1.
-log_age = 9.8
+log_age = 9.5
 
 met_isochrones = isochrones_data[np.around(isochrones_data.T[1], 2)==metallicity]
 final_isochrone = met_isochrones[np.around(met_isochrones.T[2], 2)==log_age]
@@ -78,8 +91,10 @@ g_i_isochrone = final_isochrone[:,12] - final_isochrone[:,14]
 g_isochrone = final_isochrone[:,12]
 isochrone_points = np.stack([g_i_isochrone, g_isochrone])
 
-for field in field_list:
-    if field == '4' or field == '9' or field == '1':
+for field, radius, PA in zip(field_list, field_radius, field_PA):
+    if field != 13:
+        continue
+    if field == 4 or field == 9 or field == 1:
         name_field_data = '/scratch/SMASH_DATA/Field%s_allobj_stars.fits' % (field)
     else:
         name_field_data = '/scratch/SMASH_DATA/Field%s_allobj_stars.fits.gz' % (field)
@@ -88,5 +103,5 @@ for field in field_list:
     mag_g = dereddener(smash_data['G'], 1, smash_data['EBV'], field)
     mag_i = dereddener(smash_data['I'], 3, smash_data['EBV'], field)
     distance_modulus = 18.9
-    isochrone_plotter(isochrone_points, mag_g, mag_i, distance_modulus, field)
+    isochrone_plotter(isochrone_points, mag_g, mag_i, distance_modulus, field, radius, PA)
     print('Finished with field', field)
